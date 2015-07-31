931b8bac74fbe8893217f80656ae2e04e1478779
justinccdev <justincc@justincc.org>
2015-07-31 21:04:07 +0100

    Handle UTF8 fields (sort of)

--- jjvm.py
+++ jjvm.py
@@ -17,13 +17,28 @@ class MyParser(argparse.ArgumentParser):
 ###################
 ### SUBROUTINES ###
 ###################
-def lenCpStruct(tag):
-  if tag == 7:
-    return 3
+def readToNextCpStruct(clazz):
+  tag = ord(clazz.read(1))
+
+  print "Field %d" % (tag)
+
+  remainingSeek = 0
+
+  if tag == 1:
+    # FIXME: Yes, this will blow badly on encountering anything other than ASCII
+    # https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html
+    remainingSeek = struct.unpack(">H", clazz.read(2))[0]
+  elif tag == 7:
+    remainingSeek = 2
   elif tag == 10:
-    return 5
+    remainingSeek = 4
   else:
-    return -1
+    print "ERROR: Unrecognized tag %d" % tag
+    sys.exit(1)
+  
+  print "Remaining seek %d" % remainingSeek
+
+  clazz.seek(remainingSeek, os.SEEK_CUR)
 
 ############
 ### MAIN ###
@@ -32,24 +47,13 @@ parser = MyParser('Run bytecode in jjvm')
 parser.add_argument('path', help='path to class')
 args = parser.parse_args()
 
-with open(args.path, "rb") as c:
-  c.seek(8)
-  cpCount = struct.unpack(">H", c.read(2))[0] - 1
+with open(args.path, "rb") as clazz:
+  clazz.seek(8)
+  cpCount = struct.unpack(">H", clazz.read(2))[0] - 1
   cpIndex = 1
 
   print "Constant pool count: %d" % cpCount;
 
   while cpIndex <= cpCount:
-    cpTag = ord(c.read(1))
-
-    print "Field %d: %d" % (cpIndex, cpTag)
-    cpStructSize = lenCpStruct(cpTag)
-
-    if cpStructSize < 0:
-      print "ERROR: cpStructSize %d for tag %d" % (cpStructSize, cpTag)
-      sys.exit(1)
-
-    # print "Size: %d" % cpStructSize
-  
+    readToNextCpStruct(clazz)
     cpIndex += 1
-    c.seek(cpStructSize - 1, os.SEEK_CUR)
