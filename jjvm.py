071f131d98376ffa9097233e5c31bdb1cec03e1d
justinccdev <justincc@justincc.org>
2015-08-01 10:17:27 +0100

    Print field indexes

--- jjvm.py
+++ jjvm.py
@@ -38,7 +38,7 @@ def readToNextCpStruct(clazz):
   tag = ord(clazz.read(1))
 
   # print "Tag %d %s" % (tag, TAG_NAMES[tag])
-  print "Tag %s" % (TAG_NAMES[tag]),
+  res = TAG_NAMES[tag]
 
   remainingSeek = 0
 
@@ -46,18 +46,16 @@ def readToNextCpStruct(clazz):
     # FIXME: Yes, this will blow badly on encountering anything other than ASCII
     # https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html
     strLen = readU2(clazz)
-    print clazz.read(strLen)
+    res += " "
+    res += clazz.read(strLen)
     # print struct.unpack("s", clazz.read(strLen))[0]
     # remainingSeek = readU2(clazz)
   elif tag == 7:
     remainingSeek = 2
-    print
   elif tag == 10:
     remainingSeek = 4
-    print
   elif tag == 12:
     remainingSeek = 4
-    print
   else:
     print "ERROR: Unrecognized tag %d" % tag
     sys.exit(1)
@@ -66,6 +64,8 @@ def readToNextCpStruct(clazz):
 
   if remainingSeek > 0:
     clazz.seek(remainingSeek, os.SEEK_CUR)
+
+  return res
 
 def readU2(clazz):
   return struct.unpack(">H", clazz.read(2))[0]
@@ -86,14 +86,14 @@ with open(args.path, "rb") as clazz:
 
   while cpIndex <= cpCount:
     # print "Reading field %d" % cpIndex
-    readToNextCpStruct(clazz)
+    print "%d %s" % (cpIndex, readToNextCpStruct(clazz))
     cpIndex += 1
 
   # Skip access flags, tjhis class, super class refs
   clazz.seek(6, os.SEEK_CUR)
 
   # TODO: Yes, need to deal with when these aren't actually 0!
-  print "Interfaces count: %d" % readU2(clazz)
+  print "\nInterfaces count: %d" % readU2(clazz)
   print "Fields count: %d" % readU2(clazz)
   print "Methods count: %d" % readU2(clazz)
 
