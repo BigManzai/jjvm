1c4db036b262a85115805a363a73590b6d4af4b1
BigManzai <39168019+BigManzai@users.noreply.github.com>
2018-05-19 17:33:54 +0200

    Add files via upload

---
+++ jjvm_py3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import argparse
+from jjvm.jclass import jclass
+from jjvm.jutil import readU2
+import os
+import struct
+import sys
+
+###############
+### CLASSES ###
+###############
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+###################
+### SUBROUTINES ###
+###################
+
+############
+### MAIN ###
+############
+parser = MyParser('Run bytecode in jjvm')
+parser.add_argument('path', help='path to class')
+args = parser.parse_args()
+
+clazz = jclass(args.path)
+
+for m in list(clazz.getMethods().values()):
+  print("Method name: %s" % m.getName())
+  print("Descriptor: %s" % m.getDescriptor())
+  m.printCode()
+  print()
